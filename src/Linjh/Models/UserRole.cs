20c71a570af58ffafd84760e82cfcec5018dd120
Linjh95 <2583122953@qq.com>
2017-11-02 18:19:40 +0800

    增加新文件

---
+++ src/Linjh/Models/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linjh.Models
+{
+    public class UserRole
+    {
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
